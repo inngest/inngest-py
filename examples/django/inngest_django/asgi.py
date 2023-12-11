4fe4636dc8c7572808d5a483d8d27d8f1fed61c4
Aaron Harper <aaron@inngest.com>
2023-12-11 09:13:53 -0500

    Fix lint errors

--- examples/django/inngest_django/asgi.py
+++ examples/django/inngest_django/asgi.py
@@ -1,16 +1,7 @@
-"""
-ASGI config for inngest_django project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inngest_django.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inngest_django.settings")
 
 application = get_asgi_application()
