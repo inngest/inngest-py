b23d9bb2c2b1f1464dd039f99178231c352e49bc
Aaron Harper <aaron@inngest.com>
2023-12-29 11:54:56 -0500

    Remove custom assert error outputs

--- inngest/_internal/comm_test.py
+++ inngest/_internal/comm_test.py
@@ -59,9 +59,7 @@ class Test_get_function_configs(unittest.TestCase):
         )
 
         configs = handler.get_function_configs("http://foo.bar")
-        assert not isinstance(
-            configs, Exception
-        ), f"Unexpected error: {configs}"
+        assert not isinstance(configs, Exception)
 
     def test_no_functions(self) -> None:
         functions: list[inngest.Function] = []
