13e67ec8bca18288c6226d6e861213e6ac43aa82
Aaron Harper <aaron@inngest.com>
2024-05-05 22:00:58 -0400

    Fix const version

--- inngest/_internal/const.py
+++ inngest/_internal/const.py
@@ -6,7 +6,7 @@ DEFAULT_EVENT_ORIGIN: typing.Final = "https://inn.gs/"
 DEV_SERVER_ORIGIN: typing.Final = "http://127.0.0.1:8288/"
 LANGUAGE: typing.Final = "py"
 ROOT_STEP_ID: typing.Final = "step"
-VERSION: typing.Final = "0.3.13.dev2"
+VERSION: typing.Final = "0.3.13"
 
 
 class EnvKey(enum.Enum):
