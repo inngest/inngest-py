2ecc840c1402a0ec6c05907b5c8065a48fd78ab9
Aaron Harper <aaron@inngest.com>
2024-07-24 15:22:07 -0400

    Bump version to 0.4.8 (#140)

--- inngest/_internal/const.py
+++ inngest/_internal/const.py
@@ -5,7 +5,7 @@ DEFAULT_API_ORIGIN: typing.Final = "https://api.inngest.com/"
 DEFAULT_EVENT_API_ORIGIN: typing.Final = "https://inn.gs/"
 DEV_SERVER_ORIGIN: typing.Final = "http://127.0.0.1:8288/"
 LANGUAGE: typing.Final = "py"
-VERSION: typing.Final = "0.4.7"
+VERSION: typing.Final = "0.4.8"
 
 
 class EnvKey(enum.Enum):
