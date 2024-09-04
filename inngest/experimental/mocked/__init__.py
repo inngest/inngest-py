cd7dbca7042f1e438bbdc3dfc038642c98841e1d
Aaron Harper <aaron@inngest.com>
2024-09-03 21:11:58 -0400

    Update comment

--- inngest/experimental/mocked/__init__.py
+++ inngest/experimental/mocked/__init__.py
@@ -1,5 +1,9 @@
 """
 Simulate Inngest function execution without an Inngest server.
+
+NOT STABLE! This is an experimental feature and may change in the future. If
+you'd like to depend on it, we recommend copying this directory into your source
+code.
 """
 
 from .consts import Status, Timeout
