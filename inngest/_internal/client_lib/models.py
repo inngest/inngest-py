4b503fc41a922fdb90acdf6b00731eebd7e6f6c9
Aaron Harper <aaron@inngest.com>
2024-09-23 13:17:00 -0400

    Fix send event error handling

--- inngest/_internal/client_lib/models.py
+++ inngest/_internal/client_lib/models.py
@@ -1,8 +1,10 @@
 import typing
 
+import pydantic
+
 from inngest._internal import types
 
 
 class SendEventsResult(types.BaseModel):
     error: typing.Optional[str] = None
-    ids: list[str]
+    ids: list[str] = pydantic.Field(default_factory=list)
