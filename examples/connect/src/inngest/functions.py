ead48bb56f5bc9a00d9faef2108031dbf2dac603
Aaron Harper <aaron@inngest.com>
2025-05-08 13:52:49 -0400

    Revert small example change

--- examples/connect/src/inngest/functions.py
+++ examples/connect/src/inngest/functions.py
@@ -7,10 +7,5 @@ from .client import inngest_client
     fn_id="hello-world",
     trigger=inngest.TriggerEvent(event="say-hello"),
 )
-async def hello(ctx: inngest.Context) -> None:
-    await ctx.group.parallel(
-        (
-            lambda: ctx.step.run("step-1", lambda: "do stuff"),
-            lambda: ctx.step.run("step-2", lambda: "do other stuff"),
-        )
-    )
+async def hello(ctx: inngest.Context) -> str:
+    return "Hello world!"
