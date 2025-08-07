48b1799372d00aba4eb38e6e81185a45777c266a
Aaron Harper <aaron@inngest.com>
2025-08-06 20:23:26 -0400

    Fix Connect heartbeater dying on error (#272)

--- pkg/inngest/inngest/connect/_internal/heartbeat_handler.py
+++ pkg/inngest/inngest/connect/_internal/heartbeat_handler.py
@@ -61,22 +61,33 @@ class _HeartbeatHandler(_BaseHandler):
         await self._state.ws.wait_for_not_none()
 
         while self.closed_event.is_set() is False:
-            # Only send heartbeats when the connection is active.
-            if self._state.conn_state.value != ConnectionState.ACTIVE:
-                await self._state.conn_state.wait_for(ConnectionState.ACTIVE)
+            # Use try/except to ensure that we don't stop sending heartbeats if
+            # one errors
+            try:
+                # Only send heartbeats when the connection is active.
+                if self._state.conn_state.value != ConnectionState.ACTIVE:
+                    await self._state.conn_state.wait_for(
+                        ConnectionState.ACTIVE
+                    )
 
-            # IMPORTANT: We need to get the WS conn each loop iteration because
-            # it may have changed (e.g. due to a reconnect)
-            ws = await self._state.ws.wait_for_not_none()
+                # IMPORTANT: We need to get the WS conn each loop iteration because
+                # it may have changed (e.g. due to a reconnect)
+                ws = await self._state.ws.wait_for_not_none()
 
-            self._logger.debug("Sending heartbeat")
-            await ws.send(
-                connect_pb2.ConnectMessage(
-                    kind=connect_pb2.GatewayMessageType.WORKER_HEARTBEAT,
-                ).SerializeToString()
-            )
-
+                self._logger.debug("Sending heartbeat")
+                await ws.send(
+                    connect_pb2.ConnectMessage(
+                        kind=connect_pb2.GatewayMessageType.WORKER_HEARTBEAT,
+                    ).SerializeToString()
+                )
+            except Exception as e:
+                # Don't raise the error because we want to continue heartbeating
+                self._logger.error(
+                    "Error sending heartbeat", extra={"error": str(e)}
+                )
             await asyncio.sleep(_heartbeat_interval_sec)
+
+        self._logger.debug("Heartbeater task stopped")
 
     def handle_msg(
         self,
