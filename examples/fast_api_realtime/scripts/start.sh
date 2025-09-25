50f1d409fca6e30395c2de1e5a1fa7243d4d247e
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-09-25 18:56:48 -0400

    Update to latest package for vite compat

--- examples/fast_api_realtime/scripts/start.sh
+++ examples/fast_api_realtime/scripts/start.sh
@@ -3,9 +3,12 @@ set -e
 export PYTHONPATH=${PYTHONPATH}:../..
 
 # Load env vars from .env file in repo root.
-ENV_VARS=$(cat ../../.env | grep -v ^#)
-if [ -n "${ENV_VARS}" ]; then
-    export $(echo ${ENV_VARS} | xargs)
+if [ -f ../../.env ]; then
+    echo "Loading env vars from ../../.env"
+    ENV_VARS=$(cat ../../.env | grep -v ^#)
+    if [ -n "${ENV_VARS}" ]; then
+        export $(echo ${ENV_VARS} | xargs)
+    fi
 fi
 
 uv run --project ../.. uvicorn app:app --reload
