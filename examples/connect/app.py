7b8fe4e1f3e83e972ee779c6de321ccbb0033182
Muzammil <muzammil@inngest.com>
2025-10-29 12:56:45 -0700

    Adding Example For Connect Concurrency

--- examples/connect/app.py
+++ examples/connect/app.py
@@ -2,6 +2,14 @@ import asyncio
 
 from inngest.connect import connect
 from src.inngest.client import inngest_client
-from src.inngest.functions import hello
+from src.inngest.functions import hello, hello_slow, hello_really_slow
 
-asyncio.run(connect([(inngest_client, [hello])]).start())
+# generic example
+asyncio.run(connect([(inngest_client, [hello])], max_worker_concurrency=1).start())
+
+# slow example with worker concurrency limit
+asyncio.run(
+    connect(
+        [(inngest_client_slow, [hello_slow, hello_really_slow])],
+        max_worker_concurrency=1
+    ).start())
--- examples/connect/src/inngest/functions.py
+++ examples/connect/src/inngest/functions.py
@@ -9,3 +9,20 @@ from .client import inngest_client
 )
 async def hello(ctx: inngest.Context) -> str:
     return "Hello world!"
+
+
+@inngest_client.create_function(
+    fn_id="hello-slow-world",
+    trigger=inngest.TriggerEvent(event="say-hello-slow"),
+)
+async def hello_slow(ctx: inngest.Context) -> str:
+    time.sleep(100)
+    return "Hello Slow World!"
+
+@inngest_client_slow.create_function(
+    fn_id="hello-really-slow-world",
+    trigger=inngest.TriggerEvent(event="say-hello-really-slow"),
+)
+async def hello_really_slow(ctx: inngest.Context) -> str:
+    time.sleep(10000)
+    return "Hello Really Slow World!"
